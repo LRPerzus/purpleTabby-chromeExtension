547549b7c8f708399d7095b782f986fa9dd6927f
LRPerzus <perzusmyname@gmail.com>
2024-09-10 14:02:46 +0800

    small edits to mustationObserver

--- injectScripts/attachMutationObserver.js
+++ injectScripts/attachMutationObserver.js
@@ -3,8 +3,6 @@ const callback = (mutationList, observer) => {
     for (const mutation of mutationList) {
       if (mutation.type === "childList") {
         console.log("A child node has been added or removed.");
-      } else if (mutation.type === "attributes") {
-        console.log(`The ${mutation.attributeName} attribute was modified.`);
       }
     }
   };
