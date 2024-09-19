49d260ee748856620b6cbda72046d1af15f5690e
CKodes <ckodes@outlook.com>
2024-09-19 16:34:54 +0800

    chore: prettier

--- background functions/a11yTreeFuncs.js
+++ background functions/a11yTreeFuncs.js
@@ -1,85 +1,97 @@
-export async function getFullAXTree(tabId,frameId) {
-    return new Promise((resolve, reject) => {
-        chrome.debugger.sendCommand({ tabId }, 'Accessibility.getFullAXTree', {frameId:frameId}, (result) => {
-            if (chrome.runtime.lastError) {
-                reject(new Error(chrome.runtime.lastError.message));
-            } else {
-                resolve(result);
-            }
-        });
-    });
+export async function getFullAXTree(tabId, frameId) {
+  return new Promise((resolve, reject) => {
+    chrome.debugger.sendCommand(
+      { tabId },
+      'Accessibility.getFullAXTree',
+      { frameId: frameId },
+      (result) => {
+        if (chrome.runtime.lastError) {
+          reject(new Error(chrome.runtime.lastError.message))
+        } else {
+          resolve(result)
+        }
+      }
+    )
+  })
 }
 
-export async function fullA11yTreeFilter(tabId,fullA11yTree) {
-    const backEndIdWithName = {};
-    console.log("fullA11yTreeFilter", fullA11yTree);
-    const listOfRolesThatWillBeSeen = ["button"]
-    let count = 0
-    for (const obj of fullA11yTree) {
-        /* 
+export async function fullA11yTreeFilter(tabId, fullA11yTree) {
+  const backEndIdWithName = {}
+  console.log('fullA11yTreeFilter', fullA11yTree)
+  const listOfRolesThatWillBeSeen = ['button']
+  let count = 0
+  for (const obj of fullA11yTree) {
+    /* 
             Switch to this if want to swtich back to only noticing stuff thats would not automatically be detected
             (obj.name.value !== "" || listOfRolesThatWillBeSeen.includes(obj.role.value))
             obj.role.value !== "RootWebArea") not an element we can add an attribute 
         */
-        if ((obj.name && obj.name.value !== "" && obj.name.value !== "uninteresting"  && obj.role.value !== "RootWebArea")) {
-            if (obj.role.value === "StaticText"  && obj.parentId !== "")
-            {
-                obj.backendDOMNodeId = parseInt(obj.parentId);
-                let parentNode;
-                try 
-                {
-                    parentNode= (await queryAXTreeByBackendNodeId(tabId,obj.backendDOMNodeId)).nodes;
-                    console.log("fullA11yTreeFilter update to the parentIds",obj.backendDOMNodeId);
-                    console.log("fullA11yTreeFilter parentNode",parentNode)
-                    if (parentNode.length > 0)
-                    {
-                        obj.parentId = parseInt(parentNode[0].parentId);
-                        console.log("The change",obj.parentId);
-                    }
-
-                }
-                catch(e)
-                {
-                    console.log(`Error queryAXTreeByBackendNodeId the id ${tabId,obj.backendDOMNodeId}`,e);
-                }
-               
-            }
-            let name = ""
-            if (obj.name !== undefined)
-            {
-                name = obj.name.value
-            }
-
-            backEndIdWithName[obj.backendDOMNodeId] = 
-            {
-                value:`${name} ${count}`,
-                parentId: parseInt(obj.parentId)
-            };
+    if (
+      obj.name &&
+      obj.name.value !== '' &&
+      obj.name.value !== 'uninteresting' &&
+      obj.role.value !== 'RootWebArea'
+    ) {
+      if (obj.role.value === 'StaticText' && obj.parentId !== '') {
+        obj.backendDOMNodeId = parseInt(obj.parentId)
+        let parentNode
+        try {
+          parentNode = (
+            await queryAXTreeByBackendNodeId(tabId, obj.backendDOMNodeId)
+          ).nodes
+          console.log(
+            'fullA11yTreeFilter update to the parentIds',
+            obj.backendDOMNodeId
+          )
+          console.log('fullA11yTreeFilter parentNode', parentNode)
+          if (parentNode.length > 0) {
+            obj.parentId = parseInt(parentNode[0].parentId)
+            console.log('The change', obj.parentId)
+          }
+        } catch (e) {
+          console.log(
+            `Error queryAXTreeByBackendNodeId the id ${(tabId, obj.backendDOMNodeId)}`,
+            e
+          )
         }
-        else
-        {
-            console.log("fullA11yTreeFilter cannot get xpath from element?")
-        }
-        count++;
+      }
+      let name = ''
+      if (obj.name !== undefined) {
+        name = obj.name.value
+      }
+
+      backEndIdWithName[obj.backendDOMNodeId] = {
+        value: `${name} ${count}`,
+        parentId: parseInt(obj.parentId),
+      }
+    } else {
+      console.log('fullA11yTreeFilter cannot get xpath from element?')
     }
+    count++
+  }
 
-    console.log("fullA11yTreeFilter DONE");
-    return backEndIdWithName;
+  console.log('fullA11yTreeFilter DONE')
+  return backEndIdWithName
 }
 
 async function queryAXTreeByBackendNodeId(tabId, backendNodeId) {
-    return new Promise((resolve, reject) => {
-        const params = {
-            backendNodeId // Only include backendNodeId in the parameters
-        };
+  return new Promise((resolve, reject) => {
+    const params = {
+      backendNodeId, // Only include backendNodeId in the parameters
+    }
 
-        chrome.debugger.sendCommand({ tabId }, 'Accessibility.queryAXTree', params, (result) => {
-            if (chrome.runtime.lastError) {
-                reject(new Error(chrome.runtime.lastError.message));
-                return;
-            }
+    chrome.debugger.sendCommand(
+      { tabId },
+      'Accessibility.queryAXTree',
+      params,
+      (result) => {
+        if (chrome.runtime.lastError) {
+          reject(new Error(chrome.runtime.lastError.message))
+          return
+        }
 
-            resolve(result);
-        });
-    });
+        resolve(result)
+      }
+    )
+  })
 }
--- background functions/common.js
+++ background functions/common.js
@@ -1,4 +1,4 @@
-import {getFromLocal} from "./localStorageFunc.js"
+import { getFromLocal } from './localStorageFunc.js'
 /* 
     Function to set attribute purple_tabby_a11yTree to DOM items via Debugger DOM commands
     Reason: 
@@ -13,111 +13,125 @@ import {getFromLocal} from "./localStorageFunc.js"
         True or False if its been attached (Currently broken)
 */
 export async function setAttributeValue(tabId, nodeId, name) {
-    console.log("setAttributeValue nodeId", nodeId);
+  console.log('setAttributeValue nodeId', nodeId)
 
-    try {
-        // Step 1: Set the attribute
-        await new Promise((resolve, reject) => {
-            chrome.debugger.sendCommand({ tabId }, 'DOM.setAttributeValue', {
-                nodeId: nodeId,
-                name: name,
-                value: "true"
-            }, (result) => {
-                if (chrome.runtime.lastError) {
-                    reject(new Error(chrome.runtime.lastError.message));
-                    return;
-                }
+  try {
+    // Step 1: Set the attribute
+    await new Promise((resolve, reject) => {
+      chrome.debugger.sendCommand(
+        { tabId },
+        'DOM.setAttributeValue',
+        {
+          nodeId: nodeId,
+          name: name,
+          value: 'true',
+        },
+        (result) => {
+          if (chrome.runtime.lastError) {
+            reject(new Error(chrome.runtime.lastError.message))
+            return
+          }
 
-                resolve(result);
-            });
-        });
-        return true;
-    } catch (error) {
-        if(error.message.includes("Could not find node with given id"))
-        {
-            console.warn(`Element node id does not exists, attempt to redo nodeId : ${nodeId}`);
-            return "redo"
+          resolve(result)
         }
-        else
-        {
-            console.error(`Failed to set attribute for nodeId ${nodeId}: ${error.message}`)
-        }
+      )
+    })
+    return true
+  } catch (error) {
+    if (error.message.includes('Could not find node with given id')) {
+      console.warn(
+        `Element node id does not exists, attempt to redo nodeId : ${nodeId}`
+      )
+      return 'redo'
+    } else {
+      console.error(
+        `Failed to set attribute for nodeId ${nodeId}: ${error.message}`
+      )
     }
+  }
 }
 
 /*
     Function that is used after each finished message for A11y Tree and get clickableElements to check if the scan is finished
     It checks if in the local storage stores both A11yTree and clickableElements.
 */
-export async function areScansFinished(tabId)
-{
-    let A11yTree = null; 
-    let clickAbleElements = null;
-    const currentClick = await getFromLocal(tabId,"noClicks");
-    console.log("currentClick",currentClick);
-    console.log("tabId",tabId);
-    
-    try {
-        const foundElements = await getFromLocal(tabId,"foundElements",currentClick);
-        if (foundElements && foundElements.length > 0) {
-            A11yTree = foundElements;
-            console.log('GET FROM LOCAL A11yTree:', A11yTree);
+export async function areScansFinished(tabId) {
+  let A11yTree = null
+  let clickAbleElements = null
+  const currentClick = await getFromLocal(tabId, 'noClicks')
+  console.log('currentClick', currentClick)
+  console.log('tabId', tabId)
 
-            // Do something with A11yTree
-        } else {
-            console.log('foundElements does not exist or is empty');
-        }
-    } catch (error) {
-        console.error('Error retrieving foundElements:', error);
-        // A11yTree remains null if there's an error
+  try {
+    const foundElements = await getFromLocal(
+      tabId,
+      'foundElements',
+      currentClick
+    )
+    if (foundElements && foundElements.length > 0) {
+      A11yTree = foundElements
+      console.log('GET FROM LOCAL A11yTree:', A11yTree)
+
+      // Do something with A11yTree
+    } else {
+      console.log('foundElements does not exist or is empty')
     }
+  } catch (error) {
+    console.error('Error retrieving foundElements:', error)
+    // A11yTree remains null if there's an error
+  }
 
-    // For clickable
-    try {
-        const clickable = await getFromLocal(tabId,"clickableElements",currentClick);
-        console.log("GET FROM LOCAL clickableElements", clickable)
-        if (clickable && clickable.length > 0) {
-            clickAbleElements = clickable;
-            console.log('clickableElements:', clickAbleElements);
-            // Do something with A11yTree
-        } else {
-            console.log('clickableElements does not exist or is empty');
-        }
-    } catch (error) {
-        console.error('Error retrieving clickableElements:', error);
-        // A11yTree remains null if there's an error
+  // For clickable
+  try {
+    const clickable = await getFromLocal(
+      tabId,
+      'clickableElements',
+      currentClick
+    )
+    console.log('GET FROM LOCAL clickableElements', clickable)
+    if (clickable && clickable.length > 0) {
+      clickAbleElements = clickable
+      console.log('clickableElements:', clickAbleElements)
+      // Do something with A11yTree
+    } else {
+      console.log('clickableElements does not exist or is empty')
     }
+  } catch (error) {
+    console.error('Error retrieving clickableElements:', error)
+    // A11yTree remains null if there's an error
+  }
 
-    if ( A11yTree !== null && clickAbleElements !== null)
-    {
-        console.log("YAY ITS ALL DONE");
-        const data = 
-        {
-            clickAbleElements:clickAbleElements,
-            A11yTree: A11yTree,
-            tabId, tabId
-        }
-        chrome.tabs.sendMessage(tabId, { type: "SCAN_COMEPLETE", data:data });
-
+  if (A11yTree !== null && clickAbleElements !== null) {
+    console.log('YAY ITS ALL DONE')
+    const data = {
+      clickAbleElements: clickAbleElements,
+      A11yTree: A11yTree,
+      tabId,
+      tabId,
     }
-
+    chrome.tabs.sendMessage(tabId, { type: 'SCAN_COMEPLETE', data: data })
+  }
 }
 
 export async function doubleCheckNodeId(tabId, backendNodeId) {
-    return new Promise((resolve, reject) => {
-
-        chrome.debugger.sendCommand({ tabId }, "DOM.describeNode", { backendNodeId: backendNodeId }, (result) => {
-            if (chrome.runtime.lastError) {
-                const errorMessage = `Error in doubleCheckNodeId: ${chrome.runtime.lastError.message} (tabId: ${tabId}, backendNodeId: ${backendNodeId})`;
-                console.log(errorMessage);
-                // reject(new Error(errorMessage));
-            } else if (!result || !result.node) {
-                const errorMessage = `Error in doubleCheckNodeId result || !result.node : No node found (tabId: ${tabId}, backendNodeId: ${backendNodeId})`;
-                console.log(errorMessage);
-                // reject(new Error(errorMessage));
-            } else {
-                resolve(result.node);
-            }
-        });
-    });
+  return new Promise((resolve, reject) => {
+    chrome.debugger.sendCommand(
+      { tabId },
+      'DOM.describeNode',
+      { backendNodeId: backendNodeId },
+      (result) => {
+        if (chrome.runtime.lastError) {
+          const errorMessage = `Error in doubleCheckNodeId: ${chrome.runtime.lastError.message} (tabId: ${tabId}, backendNodeId: ${backendNodeId})`
+          console.log(errorMessage)
+          // reject(new Error(errorMessage));
+        } else if (!result || !result.node) {
+          const errorMessage = `Error in doubleCheckNodeId result || !result.node : No node found (tabId: ${tabId}, backendNodeId: ${backendNodeId})`
+          console.log(errorMessage)
+          // reject(new Error(errorMessage));
+        } else {
+          resolve(result.node)
+        }
+      }
+    )
+  })
 }
